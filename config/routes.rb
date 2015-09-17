ce3e8efe4c96dd6070f803914cb678820949e24a
Eben Smith <eben.b.smith@gmail.com>
2015-09-16 18:09:15 -0600

    Changes to style sheets and added some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'store/index'
-
   resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
